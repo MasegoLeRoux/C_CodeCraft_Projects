aa251de104c1f41232b270a94abb09e5e81f5c21
Kamogelo Masego Mashigo <107093784+MasegoLeRoux@users.noreply.github.com>
2023-08-20 02:46:09 +0200

    Add files via upload

---
+++ Console Application/RockPaperScissorsGame/README.md
@@ -0,0 +1,39 @@
+# Rock-Paper-Scissors Game
+
+<p align="center">
+  <img src="https://media.giphy.com/media/vISmwpBJUNYzukTnVx/giphy.gif" alt="Project Banner" height="200" width = "1000">
+</p>
+
+Welcome to the Rock-Paper-Scissors game! This simple console application allows you to play the classic Rock-Paper-Scissors game against the computer.
+
+## How to Play
+
+1. Run the program in a C# environment (Visual Studio, Visual Studio Code, etc.).
+2. You will be prompted to enter your name. Please provide a valid name.
+3. Follow the on-screen instructions to select your move: ROCK, PAPER, or SCISSORS.
+4. The computer will randomly choose its move.
+5. The winner of the round will be announced, and the score will be displayed.
+6. The game continues until either you or the computer wins the required number of rounds.
+
+## Prerequisites
+
+- .NET Core or .NET Framework (compatible with C# programming environment)
+- Basic understanding of C# programming concepts
+
+## Running the Program
+
+1. Clone this repository to your local machine.
+2. Open the project in your preferred C# development environment.
+3. Build and run the program.
+
+## Contributing
+
+Contributions to enhance the game or fix issues are welcome! Please submit pull requests or issues if you have any suggestions or improvements.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Have fun playing Rock-Paper-Scissors against the computer! If you have any questions or feedback, feel free to reach out.
---
+++ Console Application/RockPaperScissorsGame/RockPaperScissorsGame/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+   namespace RockPaperScissorsGame {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userWins = 0;
+            int computerWins = 0;
+            int roundsToWin = 2;
+
+            Console.WriteLine("Welcome to the Rock-Paper-Scissors game!");
+
+            // Prompt user for their name and validate it
+            string userName;
+            do
+            {
+                Console.Write("Enter your name: ");
+                userName = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(userName))
+                {
+                    Console.WriteLine("Name cannot be empty. Please enter your name.");
+                }
+            } while (string.IsNullOrWhiteSpace(userName));
+
+            Console.WriteLine($"Hello, {userName}!");
+
+            do
+            {
+                Console.WriteLine("\nSelect your move:\n1->ROCK\n2->PAPER\n3->SCISSORS");
+                string[] choices = { "ROCK", "PAPER", "SCISSORS" };
+                Random random = new Random();
+                int computerChoice = random.Next(0, 3);
+
+                Console.WriteLine("Enter your choice:");
+                string userChoice = Console.ReadLine().ToUpper();
+
+                Console.WriteLine($"Computer: {choices[computerChoice]}");
+
+                int result = DetermineWinner(userChoice, choices[computerChoice]);
+
+                if (result == 1)
+                {
+                    Console.WriteLine($"Congratulations, {userName}! You win this round!");
+                    userWins++;
+                }
+
+                if (result == -1)
+                {
+                    Console.WriteLine($"Sorry, {userName}. Computer wins this round!");
+                    computerWins++;
+                }
+
+                if (result == 0)
+                {
+                    Console.WriteLine("It's a tie!");
+                }
+
+                Console.WriteLine($"Score: {userName} {userWins} - {computerWins} Computer");
+
+                if (userWins >= roundsToWin || computerWins >= roundsToWin)
+                {
+                    Console.WriteLine("Do you want to play again? (YES/NO):");
+                }
+                else
+                {
+                    Console.WriteLine("Next round...");
+                }
+
+            } while ((userWins < roundsToWin && computerWins < roundsToWin) ||
+                     Console.ReadLine().ToUpper() == "YES");
+
+            Console.WriteLine("\nGame over!");
+            if (userWins > computerWins)
+            {
+                Console.WriteLine($"Congratulations, {userName}! You win the game!");
+            }
+            else if (computerWins > userWins)
+            {
+                Console.WriteLine($"Sorry, {userName}. Computer wins the game.");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
+        }
+
+        static int DetermineWinner(string user, string computer)
+        {
+            if (user == computer)
+            {
+                return 0; // It's a tie
+            }
+            if ((user == "ROCK" && computer == "SCISSORS") ||
+                (user == "PAPER" && computer == "ROCK") ||
+                (user == "SCISSORS" && computer == "PAPER"))
+            {
+                return 1; // User wins
+            }
+            return -1; // Computer wins
+        }
+    }
+}
+ 
